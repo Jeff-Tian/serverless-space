c86fc59c9c04cd1625c37b4cb7849e2cb0a2dbfa
Jeff Tian <jeff.tian@outlook.com>
2021-09-09 20:08:02 +0800

    fix: https://github.com/mikestaub/serverless-express/issues/43\#issuecomment-904539166 Missing Authentication Token for api/ root route

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/', (req, res) => {
+  res.send('api')
+})
+
+app.get('/api/a', (req, res)=>{
+  res.send('hello, a!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
