1f7380d8f78bd361c1013abbfd61197f885e75cc
Jeff Tian <jeff.tian@outlook.com>
2021-09-15 13:26:14 +0800

    feat: graphsql setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common'
+import {GraphQLModule} from '@nestjs/graphql'
 import {CatsModule} from "./cats/cats.module"
+import {RecipesModule} from "./recipes/recipes.module"
 
 @Module({
-    imports: [CatsModule],
+    imports: [CatsModule, RecipesModule, GraphQLModule.forRoot({
+        autoSchemaFile: true, sortSchema: true, playground: true
+    })],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/common/plugins/logging.plugin.ts
@@ -0,0 +1,16 @@
+import {Plugin} from '@nestjs/graphql'
+import {
+    ApolloServerPlugin,
+} from 'apollo-server-plugin-base'
+
+@Plugin()
+export class LoggingPlugin implements ApolloServerPlugin {
+    async requestDidStart() {
+        console.log('Request started')
+        return {
+            async willSendResponse(): Promise<void> {
+                console.log('Will send response')
+            },
+        }
+    }
+}
