4a106e0f008ffbb0f8a717bc02b40c5fe52cb137
Jeff Tian <jeff.tian@outlook.com>
2021-09-26 20:27:24 +0800

    feat: add gatsby and xstate

--- src/gatsby-resources/files/index.ts
+++ src/gatsby-resources/files/index.ts
@@ -1,4 +1,5 @@
 import * as Joi from "@hapi/joi"
+import {sourceNodes} from "gatsby-source-filesystem/gatsby-node"
 
 const schema = {
     path: Joi.string(),
@@ -12,6 +13,20 @@ const read = async (context, id) => {
     }
 }
 
-const all = async (context) => [{id: JSON.stringify(context), name: 'whatever'}]
+const all = async (context) => {
+    sourceNodes({
+        reporter: {
+            panic: console.error
+        },
+        actions: {
+            createNode: () => ({})
+        },
+        emitter: {
+            on: console.log
+        },
+        createNodeId: () => 'abcd'
+    }, {path: 'src'})
+    return []
+}
 
 export {schema, read, all}
