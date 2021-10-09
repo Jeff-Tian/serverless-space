ffc005d0b3045809b051a0e80753e66187ba2823
Jeff Tian <jeff.tian@outlook.com>
2021-10-09 12:22:17 +0800

    feat: add cache and sandbox plugin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import {Module} from '@nestjs/common'
-import {GraphQLModule} from '@nestjs/graphql'
+import {GraphQLModule, getApolloServer} from '@nestjs/graphql'
+import {ApolloServerPluginCacheControl, ApolloServerPluginLandingPageLocalDefault} from 'apollo-server-core'
 import {CatsModule} from "./cats/cats.module"
 import {RecipesModule} from "./recipes/recipes.module"
 import {YuqueModule} from './yuque/yuque.module'
 
+
 @Module({
     imports: [CatsModule, RecipesModule, YuqueModule, GraphQLModule.forRoot({
-        autoSchemaFile: true, sortSchema: true, playground: true
+        autoSchemaFile: true,
+        sortSchema: true,
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault(), ApolloServerPluginCacheControl({defaultMaxAge: 60 * 60 * 24})]
     })],
 })
 export class AppModule {
