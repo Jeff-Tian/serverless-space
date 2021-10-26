45cce5614866b340c8fb08b8b911dc9294c2017e
Jeff Tian <jeff.tian@outlook.com>
2021-10-26 18:23:11 +0800

    feat: add response cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,34 @@
 import {Module} from '@nestjs/common'
-import {GraphQLModule} from '@nestjs/graphql'
+import {GqlModuleOptions, GraphQLModule} from '@nestjs/graphql'
 import {ApolloServerPluginCacheControl, ApolloServerPluginLandingPageLocalDefault} from 'apollo-server-core'
 import {CatsModule} from "./cats/cats.module"
 import {RecipesModule} from "./recipes/recipes.module"
 import {YuqueModule} from './yuque/yuque.module'
+import responseCachePlugin from 'apollo-server-plugin-response-cache'
+import {BaseRedisCache} from 'apollo-server-cache-redis'
+import Redis from 'ioredis'
 
 const ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+let graphqlOptions: GqlModuleOptions = {
+    autoSchemaFile: true,
+    sortSchema: true,
+    playground: false,
+    persistedQueries: {
+        ttl: ONE_DAY_IN_SECONDS
+    },
+    plugins: [ApolloServerPluginLandingPageLocalDefault(), ApolloServerPluginCacheControl({defaultMaxAge: ONE_DAY_IN_SECONDS}), responseCachePlugin({}),
+    ],
+}
+
+if (process.env['CACHE_URL']) {
+    graphqlOptions.cache = new BaseRedisCache({
+        client: new Redis(process.env['CACHE_URL']),
+    })
+}
+
 @Module({
-    imports: [CatsModule, RecipesModule, YuqueModule, GraphQLModule.forRoot({
-        autoSchemaFile: true,
-        sortSchema: true,
-        playground: false,
-        persistedQueries: {
-            ttl: ONE_DAY_IN_SECONDS
-        },
-        plugins: [ApolloServerPluginLandingPageLocalDefault(), ApolloServerPluginCacheControl({defaultMaxAge: ONE_DAY_IN_SECONDS})]
-    })],
+    imports: [CatsModule, RecipesModule, YuqueModule, GraphQLModule.forRoot(graphqlOptions)],
 })
 export class AppModule {
 }
