2f232e116b28e5907cb8fb4d6e237f40991acb42
Jeff Tian <jeff.tian@outlook.com>
2021-12-20 12:57:21 +0800

    fix: tests

--- e2e/yuque.e2e-spec.ts
+++ e2e/yuque.e2e-spec.ts
@@ -4,7 +4,7 @@ import request from "supertest"
 import { AppModule } from "../src/app.module"
 import nock from 'nock'
 
-describe('Zhihu', () => {
+describe('Yuque', () => {
     let app: INestApplication
 
     beforeAll(async () => {
--- src/yuque/yuque.module.ts
+++ src/yuque/yuque.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DateScalar } from '../common/scalars/date.scalar';
 import { YuqueResolver } from './yuque.resolver';
 import {YuqueService} from "./yuque.service"
+import {DynamoService} from "../dynamo/dynamo.service";
 
 @Module({
-    providers: [YuqueResolver, YuqueService, DateScalar],
+    providers: [YuqueResolver, YuqueService, DateScalar, DynamoService],
 })
 export class YuqueModule {}
