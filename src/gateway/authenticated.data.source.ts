8fe30a7278faafe857b772ff174ae411ccf9a397
Jeff Tian@Workstation <jie.tian@live.cn>
2023-01-24 18:55:35 +0800

    fix(UNI-119): check null before reading headers

--- src/gateway/authenticated.data.source.ts
+++ src/gateway/authenticated.data.source.ts
@@ -2,6 +2,6 @@ import {RemoteGraphQLDataSource} from "@apollo/gateway";
 
 export class AuthenticatedDataSource extends RemoteGraphQLDataSource {
     willSendRequest({request, context}) {
-        request.http?.headers.set('authorization', String(context.req.headers['authorization'] || context.req.headers['Authorization'] || context.req.headers['AUTHORIZATION']))
+        request.http?.headers.set('authorization', String(context.req?.headers['authorization'] || context.req?.headers['Authorization'] || context.req?.headers['AUTHORIZATION']))
     }
 }
