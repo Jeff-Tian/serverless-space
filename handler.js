256f75437fafded737efbb2248dd6842333cc3b3
Jeff Tian <jeff.tian@outlook.com>
2023-10-01 18:36:19 +0800

    feat: add queue and consumer

--- handler.js
+++ handler.js
@@ -5,18 +5,33 @@ const app = require('./index.js')
 module.exports.api = handler(app)
 
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Go Serverless v1.0! Your function executed successfully!',
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.consumer = async (event) => {
+    console.log('consuming ', event);
+
+    for (const record of event.Records) {
+        const messageAttributes = record.messageAttributes;
+        console.log(
+            "Message Attribute: ",
+            messageAttributes.AttributeName.stringValue
+        );
+        console.log("Message Body: ", record.body);
+    }
+
+    console.log('consumed event');
+}
