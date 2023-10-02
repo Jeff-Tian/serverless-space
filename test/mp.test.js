2f0a625a57dbc849a164f1314d8c24fa57cca398
Jeff Tian <jeff.tian@outlook.com>
2023-10-02 15:52:11 +0800

    feat: add mp add draft feature

--- test/mp.test.js
+++ test/mp.test.js
@@ -4,22 +4,83 @@ const nock = require('nock')
 const mp = require('../wechat/mp')
 
 describe("mp features", () => {
-    it('gets access token', async () => {
-        const expectedResult = {
-            "access_token": "73_I5O2Ts19YxIGqUekrHhKU4MgUgzCZb6a0VzMFF38_S8SDy5-El7bqDrs-27oNJipeAqar0mJ_McSi3efvXJ0Pr8XPSwpQf6bUROy42MbRVaGEx4ENxAWPh4mmUcYZMgAIAHIV",
-            "expires_in": 7200
-        }
+    const mockTokenRes = {
+        "access_token": "73_I5O2Ts19YxIGqUekrHhKU4MgUgzCZb6a0VzMFF38_S8SDy5-El7bqDrs-27oNJipeAqar0mJ_McSi3efvXJ0Pr8XPSwpQf6bUROy42MbRVaGEx4ENxAWPh4mmUcYZMgAIAHIV",
+        "expires_in": 7200
+    }
 
+    it('gets access token', async () => {
         nock('https://api.weixin.qq.com').post('/cgi-bin/stable_token', {
             grant_type: 'client_credential',
             appid: '1234',
             secret: '5678',
             force_refresh: false
-        }).reply(200, expectedResult)
+        }).reply(200, mockTokenRes)
 
         const res = await mp.getAccessToken();
 
         assert.ok(res);
-        assert.deepEqual(res.data, expectedResult);
+        assert.deepEqual(res.data, mockTokenRes);
+    })
+
+    it('adds an image', async () => {
+        nock('https://api.weixin.qq.com').post('/cgi-bin/stable_token', {
+            grant_type: 'client_credential',
+            appid: '1234',
+            secret: '5678',
+            force_refresh: false
+        }).reply(200, mockTokenRes)
+
+        nock('https://api.weixin.qq.com')
+            .filteringRequestBody(/.*/, '*')
+            .post(`/cgi-bin/media/upload?access_token=${mockTokenRes.access_token}&type=image`, '*').reply(200, {
+                "created_at": 1696231450,
+                "item": [],
+                "media_id": "OTC24v0nm33p4vMTERlCO-SXXxAD5Loj15meGTZJmLqpoMJhAXPJ8ebFxGoueWmW",
+                "type": "image"
+            })
+
+        const res = await mp.addTempImage();
+        assert.ok(res);
+        assert.deepEqual(res.data, {
+            "created_at": 1696231450,
+            "item": [],
+            "media_id": "OTC24v0nm33p4vMTERlCO-SXXxAD5Loj15meGTZJmLqpoMJhAXPJ8ebFxGoueWmW",
+            "type": "image"
+        });
+    })
+
+    it('adds a draft', async () => {
+        nock('https://api.weixin.qq.com').post('/cgi-bin/stable_token', {
+            grant_type: 'client_credential',
+            appid: '1234',
+            secret: '5678',
+            force_refresh: false
+        }).reply(200, mockTokenRes)
+
+        nock('https://api.weixin.qq.com')
+            .filteringRequestBody(/.*/, '*')
+            .post(`/cgi-bin/material/add_material?access_token=${mockTokenRes.access_token}&type=image`, '*')
+            .reply(200, {})
+
+        nock('https://api.weixin.qq.com')
+            .filteringRequestBody(/.*/, '*')
+            .post(`/cgi-bin/draft/add?access_token=${mockTokenRes.access_token}`)
+            .reply(200, {
+                "item": [],
+                "media_id": "H3tPf5wuG9Gu8tD8v7bpjWB8seLdmWdiF_LpGF0-wQzCU8XBbfiwOYgRxd8qzGqu"
+            })
+
+        const res = await mp.addDraft({
+            title: 'title',
+            content: 'content',
+            need_open_comment: 1
+        });
+
+        assert.ok(res);
+        assert.deepEqual(res.data, {
+            "item": [],
+            "media_id": "H3tPf5wuG9Gu8tD8v7bpjWB8seLdmWdiF_LpGF0-wQzCU8XBbfiwOYgRxd8qzGqu"
+        });
     })
 })
--- wechat/mp.js
+++ wechat/mp.js
@@ -1,4 +1,7 @@
 const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const FormData = require('form-data')
 
 module.exports.getAccessToken = async () => {
     return axios.post('https://api.weixin.qq.com/cgi-bin/stable_token', {
@@ -7,4 +10,68 @@ module.exports.getAccessToken = async () => {
         secret: '5678',
         force_refresh: false
     })
+}
+
+module.exports.addTempImage = async (access_token) => {
+    if (!access_token) {
+        const token = await module.exports.getAccessToken();
+        access_token = token.data.access_token;
+    }
+
+    const formData = new FormData();
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.png')));
+
+    console.log('uploading with token ', access_token);
+
+    return axios.post(
+        `https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${access_token}&type=image`,
+        formData,
+        {
+            headers: {
+                ...formData.getHeaders()
+            }
+        }
+    )
+}
+
+module.exports.addPersistedImage = async (access_token) => {
+    if (!access_token) {
+        const token = await module.exports.getAccessToken();
+        access_token = token.data.access_token;
+    }
+
+    const formData = new FormData();
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.png')));
+
+    console.log('uploading persisted image with token ', access_token);
+
+    return axios.post(
+        `https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=${access_token}&type=image`,
+        formData,
+        {
+            headers: {
+                ...formData.getHeaders()
+            }
+        }
+    )
+}
+
+module.exports.addDraft = async ({ title, content, content_source_url }) => {
+    const token = await module.exports.getAccessToken();
+
+    const mediaRes = await module.exports.addPersistedImage(token.data.access_token);
+
+    console.log('mediaRes= ', mediaRes.data.media_id);
+    
+    return axios.post(`https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token.data.access_token}`, {
+        articles: [
+            {
+                title,
+                content,
+                content_source_url,
+                need_open_comment: 1,
+                thumb_media_id: mediaRes.data.media_id
+            }
+        ]
+    });
 }
