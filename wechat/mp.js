149ef265c83b87d281cc6d2f24183e08e086dde1
Jeff Tian <jeff.tian@outlook.com>
2023-11-17 19:08:48 +0800

    feat: add blog image

--- wechat/mp.js
+++ wechat/mp.js
@@ -21,7 +21,7 @@ module.exports.addTempImage = async (access_token) => {
     }
 
     const formData = new FormData();
-    formData.append('media', fs.createReadStream(path.join(__dirname, './test.png')));
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
 
     console.log('uploading with token ', access_token);
 
@@ -45,12 +45,36 @@ module.exports.addPersistedImage = async (access_token) => {
     }
 
     const formData = new FormData();
-    formData.append('media', fs.createReadStream(path.join(__dirname, './test.png')));
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
 
     console.log('uploading persisted image with token ', access_token);
 
     return axios.post(
         `https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=${access_token}&type=image`,
+        formData,
+        {
+            headers: {
+                ...formData.getHeaders()
+            }
+        }
+    )
+}
+
+module.exports.addPersistedBlogImage = async (access_token, blogId) => {
+    console.log('adding image with token ', access_token);
+    if (!access_token) {
+        const token = await module.exports.getAccessToken();
+        console.log('token result = ', token.data);
+        access_token = token.data.access_token;
+    }
+
+    const formData = new FormData();
+    formData.append('media', fs.createReadStream(path.join(__dirname, './test.jpeg')));
+
+    console.log('uploading persisted image with token ', access_token);
+
+    return axios.post(
+        `https://api.weixin.qq.com/cgi-bin/media/uploadimg?access_token=${access_token}&type=image`,
         formData,
         {
             headers: {
@@ -91,13 +115,16 @@ module.exports.addDrafts = async ({title, html, markdown, content_source_url, mi
 
     console.log('mediaRes= ', mediaRes.data.media_id);
 
+    const miniprogramCoverImageRes = await module.exports.addPersistedBlogImage(token.data.access_token);
+    console.log('miniprogramCoverImageRes = ', miniprogramCoverImageRes.data);
+
     return await Promise.all([
         axios.post(`https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token.data.access_token}`, {
             articles: [
                 {
                     title,
                     author: '哈德韦',
-                    content: `${convertToHtml(markdown)}<mp-miniprogram data-miniprogram-appid="wx8c777d630f2b78e3" data-miniprogram-path="${mini_program_path}" data-miniprogram-title="在小程序中查看本文" data-miniprogram-imageurl="https://lensdump.com/i/mcS2av"></mp-miniprogram>`,
+                    content: `${convertToHtml(markdown)}<mp-miniprogram data-miniprogram-appid="wx8c777d630f2b78e3" data-miniprogram-path="${mini_program_path}" data-miniprogram-title="在小程序中查看本文" data-miniprogram-imageurl="${miniprogramCoverImageRes.data.url}"></mp-miniprogram>`,
                     content_source_url,
                     need_open_comment: 1,
                     thumb_media_id: mediaRes.data.media_id
