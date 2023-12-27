a7d9033f81ff444d8311c69d49696f3d4c65d7b8
Jeff Tian <jeff.tian@outlook.com>
2023-12-27 12:56:21 +0800

    refactor: delete unused code

--- wechat/markdown.js
+++ wechat/markdown.js
@@ -4,8 +4,6 @@ const md = require('markdown-it')({
     html: true, // 语雀生成的 markdown 里会包含 html 标签，所以这里需要开启
 })
 
-const sanitize = raw => raw.replace(/<br\s+\/?>/g, '\n\n');
-
 module.exports.convertToHtml = (markdown) => {
 
     return md.render(markdown);
