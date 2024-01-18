53a25bcaa2adb5d66949b9476656bdfba7521ab4
Jeff Tian <jeff.tian@outlook.com>
2024-01-18 19:13:37 +0800

    feat(UNI-189): 在联邦之前，检查子图是否活着，只联邦活着的子图

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,33 +1,50 @@
-import { IntrospectAndCompose } from '@apollo/gateway';
-import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { AuthenticatedDataSource } from "./authenticated.data.source";
+import {IntrospectAndCompose} from '@apollo/gateway';
+import {ApolloGatewayDriver, ApolloGatewayDriverConfig} from '@nestjs/apollo';
+import {Module} from '@nestjs/common';
+import {GraphQLModule} from '@nestjs/graphql';
+import {AuthenticatedDataSource} from "./authenticated.data.source";
+
+const allPossibleGraphs = [
+    {name: 'orders', url: 'https://uni-orders-jeff-tian.cloud.okteto.net/graphql'},
+    {name: 'face-swap', url: 'https://face-swap-jeff-tian.cloud.okteto.net/graphql'},
+    {name: 'sls', url: 'https://zcjk76jr21.execute-api.us-east-1.amazonaws.com/stg/nest/graphql'},
+    {
+        name: 'user-service',
+        url: 'https://brickverse-user-service-gracewen1.cloud.okteto.net/graphql'
+    }
+];
 
 @Module({
     imports: [
-        GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+        GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
             driver: ApolloGatewayDriver,
-            server: {
-                // ... Apollo server options
-                path: '/stg/gateway',
-            },
-            gateway: {
-                supergraphSdl: new IntrospectAndCompose({
-                    subgraphs: [
-                        { name: 'orders', url: 'https://uni-orders-jeff-tian.cloud.okteto.net/graphql' },
-                        { name: 'face-swap', url: 'https://face-swap-jeff-tian.cloud.okteto.net/graphql' },
-                        { name: 'sls', url: 'https://zcjk76jr21.execute-api.us-east-1.amazonaws.com/stg/nest/graphql' },
-                        {
-                            name: 'user-service',
-                            url: 'https://brickverse-user-service-gracewen1.cloud.okteto.net/graphql'
+            useFactory: async () => {
+                const aliveGraphs = await Promise.all(allPossibleGraphs.map(async (graph) => {
+                    try {
+                        const response = await fetch(graph.url);
+                        console.log(`Checking ${graph.url}...  ${response.status}`);
+                        return response.status < 500 ? graph : null;
+                    } catch (e) {
+                        console.error(`Error when checking ${graph.url}`);
+                        return null;
+                    }
+                })).then(graphs => graphs.filter(Boolean));
+
+                return ({
+                    server: {
+                        // ... Apollo server options
+                        path: '/stg/gateway',
+                    },
+                    gateway: {
+                        supergraphSdl: new IntrospectAndCompose({
+                            subgraphs: aliveGraphs,
+                            subgraphHealthCheck: false,
+                        }),
+                        buildService: ({name, url}) => {
+                            return new AuthenticatedDataSource({url})
                         }
-                    ],
-                    subgraphHealthCheck: false,
-                }),
-                buildService: ({ name, url }) => {
-                    return new AuthenticatedDataSource({ url })
-                }
+                    },
+                });
             },
         }),
     ],
